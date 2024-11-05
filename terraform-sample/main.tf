98cabcf227ecaa917e223e3e15959bd7e3aa2188
raziqtechnologies <81863529+raziqtechnologies@users.noreply.github.com>
2024-11-05 09:46:13 +0000

    release removed

--- terraform-sample/main.tf
+++
@@ -1,9 +0,0 @@
-# Create an EC2 instance
-resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "Terraform-Example"
-  }
-}
